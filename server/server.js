3637f0c750e297896daed0bac940938ca222e229
Suprabhat Sahoo <suprabhatbb62@gmail.com>
2024-06-12 13:10:01 +0530

    server

--- server/server.js
+++ server/server.js
@@ -3,15 +3,16 @@ const express=require("express");
 const  cors=require("cors");
 const app=express();
 const port=process.env.PORT || 8000;
-const contactrouter=require("./router/contactrouter");
-const corsOptions={
-    origin:"http://localhost:3000",
-    method:"GET, POST, PUT, DELETE, PATCH, HEAD",
-    credentials:true,
-};
-app.use(cors(corsOptions));
-app.use(express.json());
 const connectDb=require("./utils/db");
+const contactrouter=require("./router/contactrouter");
+app.use(express.json());
+// const corsOptions={
+//     origin:"http://localhost:3000",
+//     method:"GET, POST, PUT, DELETE, PATCH, HEAD",
+//     credentials:true,
+// };
+app.use(cors());
+
 app.use("/api/form", contactrouter);
 connectDb().then(()=>{
     app.listen(port, ()=>{
